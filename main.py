88206d5944f656dedaf4675c90899057b1be9be3
Omar Hbibi <omarhbibi.pro@gmail.com>
2025-11-30 18:36:08 +0100

    experimenting in jupyter notebooks, data ingestion stage

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from salesRegressor import logger
+from salesRegressor.pipeline.DataIngest import DataIngestionTrainingPipeline
 
-logger.info("My custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/salesRegressor/utils/common.py
+++ src/salesRegressor/utils/common.py
@@ -4,14 +4,11 @@ import yaml
 from salesRegressor import logger
 import json
 import joblib
-from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
 
-
-@ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
 
     try:
@@ -24,8 +21,6 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     except Exception as e:
         raise e
 
-
-@ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
 
     for path in path_to_directories:
@@ -33,8 +28,6 @@ def create_directories(path_to_directories: list, verbose=True):
         if verbose:
             logger.info(f"created directory at: {path}")
 
-
-@ensure_annotations
 def save_json(path: Path, data: dict):
 
     with open(path, "w") as f:
@@ -42,8 +35,6 @@ def save_json(path: Path, data: dict):
 
     logger.info(f"json file saved at: {path}")
 
-
-@ensure_annotations
 def load_json(path: Path) -> ConfigBox:
 
     with open(path) as f:
@@ -52,23 +43,17 @@ def load_json(path: Path) -> ConfigBox:
     logger.info(f"json file loaded succesfully from: {path}")
     return ConfigBox(content)
 
-
-@ensure_annotations
 def save_bin(data: Any, path: Path):
  
     joblib.dump(value=data, filename=path)
     logger.info(f"binary file saved at: {path}")
 
-
-@ensure_annotations
 def load_bin(path: Path) -> Any:
 
     data = joblib.load(path)
     logger.info(f"binary file loaded from: {path}")
     return data
 
-
-@ensure_annotations
 def get_size(path: Path) -> str:
 
     size_in_kb = round(os.path.getsize(path)/1024)
